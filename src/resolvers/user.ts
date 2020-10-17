78a914b1732ca61341b7b44315e3d605255df523
dcorso21 <david.l.corson@gmail.com>
2020-10-17 18:46:57 -0400

    hashed password for users

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,24 +1,29 @@
-import { User } from "src/entities/User";
+import { User } from "../entities/User";
 import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import argon2 from "argon2";
 
 @InputType()
 class UsernamePasswordInput {
     @Field()
-    username: string;
+    username!: string; // Bang sign means that you dont have to declare in the constructor
     @Field()
-    password: string;
+    password!: string;
 }
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => String)
+    @Mutation(() => User)
     async register(
         @Arg("options") options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
     ) {
-        const user = em.create(User, { username: options.username });
+        const hashedPass = await argon2.hash(options.password);
+        const user = em.create(User, {
+            username: options.username,
+            password: hashedPass,
+        });
         await em.persistAndFlush(user);
-        return "bye";
+        return user;
     }
 }
